a74dd92adbb070dc713ded6bc6133ed6b6c80eab
Jeremy Kun <j2kun@users.noreply.github.com>
2024-02-22 13:30:52 -0800

    Add prime-finding helper and test

---
+++ vanilla_bgv/number_theory.py
@@ -0,0 +1,20 @@
+import math
+import galois
+
+
+def find_ntt_prime(num_bits: int, m: int) -> int:
+  """Find the first prime q satisfying q = 1 mod m.
+
+  Args:
+    num_bits: the number of bits required in the result.
+    m: the modulus of the required equivalence, should be a power of 2.
+
+  Returns:
+    The discovered prime, or raise a ValueError if none could be found.
+  """
+  # smallest k such that m*k + 1 has at least num_bits many bits.
+  k = int(math.ceil((2**num_bits - 1) / m))
+  current = m * k + 1
+  while not galois.is_prime(current):
+    current += m
+  return current
---
+++ vanilla_bgv/number_theory_test.py
@@ -0,0 +1,19 @@
+from hypothesis import given
+from hypothesis import strategies as st
+from vanilla_bgv.number_theory import find_ntt_prime
+
+
+def test_find_ntt_prime():
+  num_bits = 4
+  m = 8
+  assert find_ntt_prime(num_bits, m) == 17
+
+
+@given(
+    st.integers(min_value=4, max_value=128),
+    st.integers(min_value=5, max_value=32),
+)
+def test_find_ntt_prime_satisfies_modulus_req(num_bits, log2_m):
+  m = 2**log2_m
+  actual = find_ntt_prime(num_bits, m)
+  assert actual % m == 1
