6e9f8e672bd25d30f2beaa758a606f9f66971961
Jeremy Kun <j2kun@users.noreply.github.com>
2024-02-22 15:44:32 -0800

    move the ntt_prime to a cached property

--- vanilla_bgv/encoding.py
+++ vanilla_bgv/encoding.py
@@ -7,8 +7,6 @@ In BGV the plaintext space is the ring of polynomials
 where N is a power of 2 and t > 2 is a prime.
 """
 
-import math
-
 import galois
 import numpy as np
 from vanilla_bgv import number_theory
@@ -18,9 +16,7 @@ from vanilla_bgv.types import Plaintext
 
 def encode(message: list[int], params: BGVParams) -> np.ndarray:
   """Encode a message as a polynomial in the plaintext space."""
-  num_bits = int(math.ceil(math.log2(params.plaintext_coeff_modulus)))
-  ntt_modulus = number_theory.find_ntt_prime(num_bits, params.poly_mod_degree)
-  prime_field = galois.GF(ntt_modulus)
+  prime_field = galois.GF(params.ntt_modulus)
   evaluation_points = prime_field(message)
 
   # Do an inverse ntt because the inputs ints are interpreted as evaluations of
@@ -28,17 +24,15 @@ def encode(message: list[int], params: BGVParams) -> np.ndarray:
   return galois.intt(
       evaluation_points,
       size=params.poly_mod_degree,
-      modulus=ntt_modulus,
+      modulus=params.ntt_modulus,
   )
 
 
 def decode(plaintext: Plaintext, params: BGVParams) -> list[int]:
   """Decode a message to a list of integers."""
-  num_bits = int(math.ceil(math.log2(params.plaintext_coeff_modulus)))
-  ntt_modulus = number_theory.find_ntt_prime(num_bits, params.poly_mod_degree)
-  prime_field = galois.GF(ntt_modulus)
+  prime_field = galois.GF(params.ntt_modulus)
   return galois.ntt(
       plaintext,
       size=params.poly_mod_degree,
-      modulus=ntt_modulus,
+      modulus=params.ntt_modulus,
   ).tolist()
--- vanilla_bgv/params.py
+++ vanilla_bgv/params.py
@@ -1,6 +1,9 @@
 """Parameter containers."""
 
 from dataclasses import dataclass
+from functools import cached_property
+import math
+from vanilla_bgv import number_theory
 
 
 @dataclass(frozen=True)
@@ -9,3 +12,8 @@ class BGVParams:
 
   poly_mod_degree: int  # referred to as N, a power of 2
   plaintext_coeff_modulus: int  # referred to as t, a prime
+
+  @cached_property
+  def ntt_modulus(self):
+    num_bits = int(math.ceil(math.log2(self.plaintext_coeff_modulus)))
+    return number_theory.find_ntt_prime(num_bits, self.poly_mod_degree)
